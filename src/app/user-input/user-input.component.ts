e51fe87b34468c568ebbb4a2648bed3be9d79e0c
FerozShaik96 <ferozshaik1997@gmail.com>
2024-12-18 00:20:57 +0530

    Resetting the input fields

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -7,22 +7,25 @@ import { AppService } from '../ap.service';
   standalone: true,
   imports: [FormsModule],
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.css'
+  styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
-  enteredInitialInvestment="0"
-  enteredAnnualInvestment="0"
-  enteredExpectedReturns="5"
-  enteredDuration="10"
-  private userInput=inject(AppService)
-  handlerSubmit(){
-    const userData={
-      enteredInitialInvestment:+this.enteredInitialInvestment,
-      enteredAnnualInvestment:Number(this.enteredAnnualInvestment),
-      enteredExpectedReturns:Number(this.enteredExpectedReturns),
-      enteredDuration:Number(this.enteredDuration)
-    }
-    this.userInput.calculateInvestmentResults(userData)
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturns = '5';
+  enteredDuration = '10';
+  private userInput = inject(AppService);
+  handlerSubmit() {
+    const userData = {
+      enteredInitialInvestment: +this.enteredInitialInvestment,
+      enteredAnnualInvestment: Number(this.enteredAnnualInvestment),
+      enteredExpectedReturns: Number(this.enteredExpectedReturns),
+      enteredDuration: Number(this.enteredDuration),
+    };
+    this.userInput.calculateInvestmentResults(userData);
+    this.enteredInitialInvestment = '0';
+    this.enteredAnnualInvestment = '0';
+    this.enteredExpectedReturns = '5';
+    this.enteredDuration = '10';
   }
-
 }
